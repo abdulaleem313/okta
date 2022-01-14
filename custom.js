925fea695676f0bb4ba56211646566dcc54b3a92
abdul <abdulaleem313@yahoo.com>
2022-01-14 23:05:06 +0500

    add event

--- custom.js
+++ custom.js
@@ -34,9 +34,9 @@ function showRegisterBtn() {
         node.appendChild(a);
         
         document.getElementById("help-links-container").appendChild(node);
-        // document.getElementById("help-links-container").addEventListener('click', event => {
-        //     document.getElementById("help-links-container").appendChild(node);
-        // });
+        document.getElementById("help-links-container").addEventListener('click', event => {
+            document.getElementById("help-links-container").appendChild(node);
+        });
     }
 }
 setTimeout(() => {
